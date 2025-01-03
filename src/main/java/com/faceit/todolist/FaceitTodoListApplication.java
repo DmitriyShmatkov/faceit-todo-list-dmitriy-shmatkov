55f8293aba652ca892748c462facaf75977e0e34
Dmitriy Shmatkov <d.shmatkov.it@gmail.com>
2025-01-03 04:33:20 +0200

    Init

---
+++ src/main/java/com/faceit/todolist/FaceitTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.faceit.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceitTodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceitTodoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/faceit/todolist/FaceitTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faceit.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceitTodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
